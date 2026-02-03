725223064292e955e99652fc6f0e73af43f72fc8
Iggor Paz <iggorpaz@gmail.com>
2026-02-03 08:36:34 -0300

    alterou nome no css productsStock

--- src/Pages/Admin/Products/ProductsStock.jsx
+++ src/Pages/Admin/Products/ProductsStock.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
-import styles from "./ProductStock.module.css";
+import styles from "./ProductsStock.module.css";
 
 export default function ProductStock() {
   const navigate = useNavigate();
